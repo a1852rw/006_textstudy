5f161615a219ed2e6baa0cb0443cef286c9c2cba
a1852rw <a1852rw@aiit.ac.jp>
2020-04-30 17:47:48 +0900

    条件分岐まで進んだ
    本日はここまで、一度コミットする

--- 001_Start_Programing_in_High_School/002_Chapter2/001_workspace/001_js-grammar/logic_002.js
+++ 001_Start_Programing_in_High_School/002_Chapter2/001_workspace/001_js-grammar/logic_002.js
@@ -1,13 +1,17 @@
 (function(){
     'use strict';
     var age = 16;
+    var isFemale = true;
     var result = null;
-    if (age >= 18) {
-        // age >= 18が真の時に実行
-        result  = '成年'
+    if (age <= 15) {
+        // age <= 15が真の時に実行
+        result  = '800円'
+    } else if (isFemale) {
+        // sex = femaleが真の時に実行
+        result = '1,000円'
     } else {
-        // age >= 18が偽の時に実行
-        result = '未成年'
+        //それ以外の場合 = 未成年でも女性でもない場合
+        result = '1,800円'
     }
     document.write(result);
 })();
